365abd5095567f39a4053087437d695dcf7881d0
raffaella26 <chenchen9724@gmail.com>
2026-07-22 20:18:44 +0800

    Delete yan-skincare-github-upload directory

--- yan-skincare-github-upload/app/main.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Home from "./page";
-import "./globals.css";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode><Home /></React.StrictMode>,
-);
